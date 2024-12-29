3fa5affb16c1780c9eec68e6c7bbf5e3c2639a9e
Douglas Silva <douglasitpro@gmail.com>
2024-12-29 17:22:13 -0300

    Caso não tenha imagem cadastrada no banco, vai enviar a imagem da pasta static no lugar

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.templatetags.static import static
+
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
