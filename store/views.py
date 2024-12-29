1b702adb7dda15f514536fcc16acc717ce8f42ae
Douglas Silva <douglasitpro@gmail.com>
2024-12-29 18:01:02 -0300

    Criando logica na função CART, para rederizarmos o carrinho, verifica se o usuário esta logado ou não passndo comportamentos diferentes para ambos

--- store/views.py
+++ store/views.py
@@ -7,7 +7,19 @@ def store(request):
     return render(request, 'store/store.html',context)
 
 def cart(request):
-    context = {}
+
+    #verificando se usuário esta logado
+    if request.user.is_authenticated:
+        #consultando os itens do carrinho
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #criando carrinho vazio, para usuário não LOGADO, pois o nosso model vai dar um loop nos itens e precisamos passar algo []
+        item = []
+        
+    #passsando itens consultado pelo contexto para o template
+    context = {'items':items}   
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
