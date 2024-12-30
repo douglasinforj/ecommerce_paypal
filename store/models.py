7de6a60fae8d81d09ddf4f2752d7aca9b6cf7c45
Douglas Silva <douglasitpro@gmail.com>
2024-12-30 15:45:59 -0300

    criando metodos para calculos de totais

--- store/models.py
+++ store/models.py
@@ -37,6 +37,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_cart_total(self):
+         orderitems = self.orderitem_set.all()
+         total = sum([item.get_total for item in orderitems])
+         return total
+    
+
+
+
+
+
     
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
