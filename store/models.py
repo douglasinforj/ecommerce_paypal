e5871ada40dc374d07816c769588468d89f69343
Douglas Silva <douglasitpro@gmail.com>
2024-12-30 23:45:32 -0300

    erro de digitação, corrigindo

--- store/models.py
+++ store/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
         return self.name
     
     #Evitar problemas ao chamar a url diretament pela url
-    @property
+    @property   
     def imageURL(self):
         try:
             url = self.image.url
@@ -33,12 +33,12 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
-    tansaction_id = models.CharField(max_length=100, null=True)
+    transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return str(self.id)
 
-    @property
+    @property  #podemos chamar diretamente da template a função
     def get_cart_total(self):
          orderitems = self.orderitem_set.all()
          total = sum([item.get_total for item in orderitems])
@@ -46,7 +46,7 @@ class Order(models.Model):
     
     @property
     def get_cart_items(self):
-         orderitems = self.orderitem_sel.all()
+         orderitems = self.orderitem_set.all()
          total = sum([item.quantity for item in orderitems])
     
 
