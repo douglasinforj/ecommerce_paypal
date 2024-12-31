c023324f37450df0a9838231b3aab4f0efbbc5e2
Douglas Silva <douglasitpro@gmail.com>
2024-12-31 04:46:05 -0300

    Vamos criar logica para mostrar quantidade de carrinho no ICON de Carrino no template, Adicionando a logica de Usuário logado a cada views para renderização como foi feito para Views Cart

--- store/views.py
+++ store/views.py
@@ -5,9 +5,20 @@ from django.http import JsonResponse
 import json
 
 def store(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        
+
     products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'store/store.html',context)
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
 
@@ -29,7 +40,17 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
 
 
