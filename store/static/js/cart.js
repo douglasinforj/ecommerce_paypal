5bdde57ba8a3630d4c793b96b5102792cd14fa4e
Douglas Silva <douglasitpro@gmail.com>
2024-12-31 02:25:37 -0300

    Adicionar ao manipulador de evento se usuário esta logado ou não(status) e informando o usuário

--- store/static/js/cart.js
+++ store/static/js/cart.js
@@ -1,4 +1,4 @@
-console.log('Teste');
+
 
 var updateBtns = document.getElementsByClassName('update-cart') 
  for (i = 0; i < updateBtns.length; i++) {
@@ -6,5 +6,12 @@ var updateBtns = document.getElementsByClassName('update-cart')
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:', user)
+        if (user == 'AnonymousUser'){
+            console.log('Usuário não autenticado')
+        }else{
+            console.log('Usuário esta autenticado, enviar dados para o banco')
+        }
     })
  }
